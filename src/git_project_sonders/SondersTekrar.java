0d6fab44f3ac1890de7ee2ac2a00b665279a3008
yasen911 <yasenarslan@gmail.com>
2020-08-16 22:10:09 +0200

    feature

--- src/git_project_sonders/SondersTekrar.java
+++ src/git_project_sonders/SondersTekrar.java
@@ -5,7 +5,7 @@ public class SondersTekrar {
 	public static void main(String[] args) {
 		
 		System.out.println("21:55 - master branch");
-
+		System.out.println("21:55 - feature branch");
 	}
 
 }
